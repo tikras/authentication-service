f6d5f2b8f6194684b2af4895fda626c440874819
Audrius <audriusgurskis@gmail.com>
2017-06-10 15:26:19 +0300

    Revert "For OC"
    This reverts commit c33a50aee45e5f2f09c6112bfabbb53f35c4a280.

--- src/main/java/com/socialbook/auth/AuthenticationApplication.java
+++ src/main/java/com/socialbook/auth/AuthenticationApplication.java
@@ -2,21 +2,14 @@ package com.socialbook.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties
-public class AuthenticationApplication extends SpringBootServletInitializer {
+public class AuthenticationApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AuthenticationApplication.class);
     }
 }
 
